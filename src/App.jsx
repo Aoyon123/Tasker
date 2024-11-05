d4ed8081b0b133dfa309390a628a761d49124ec3
Aoyon123 <fahamidulaoyon@gmail.com>
2024-11-05 22:13:28 +0600

    Header Footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './Footer'
+import Header from './Header'
+import HeroSection from './HeroSection'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      
+      <Header />
+      <HeroSection />
+      <Footer />
     </>
   )
 }
