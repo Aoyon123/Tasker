234a64a71b72346ffce6662e19321ca017a960c2
Aoyon123 <fahamidulaoyon@gmail.com>
2024-11-09 10:10:45 +0600

    Task List,Add Task

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Footer from './Footer'
 import Header from './Header'
 import HeroSection from './HeroSection'
+import TaskBoard from './task/TaskBoard'
 
 function App() {
  
@@ -9,7 +10,11 @@ function App() {
   return (
     <>
       <Header />
+      <div className="flex flex-col items-center justify-center">
       <HeroSection />
+      <TaskBoard />
+      </div>
+      
       <Footer />
     </>
   )
